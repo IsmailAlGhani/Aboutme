262df2a12031dd70ae28965a8c6b9592055d2e25
Ismail Al Ghani <ismailalghani@gmail.com>
2019-04-13 10:36:57 +0700

    Updates

---
+++ precache-manifest.753972d031b16bf9c8e187ab5b835a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "913de64804797b58a006",
+    "url": "/Aboutme/static/js/runtime~main.07e9213f.js"
+  },
+  {
+    "revision": "a705b5361eae00f5e942",
+    "url": "/Aboutme/static/js/main.bda4d2e5.chunk.js"
+  },
+  {
+    "revision": "0f8bd219b3991b89c585",
+    "url": "/Aboutme/static/js/2.4a346a31.chunk.js"
+  },
+  {
+    "revision": "a705b5361eae00f5e942",
+    "url": "/Aboutme/static/css/main.d6c455b6.chunk.css"
+  },
+  {
+    "revision": "c4c8837d5027df11edca9583c34fe53f",
+    "url": "/Aboutme/index.html"
+  }
+];
